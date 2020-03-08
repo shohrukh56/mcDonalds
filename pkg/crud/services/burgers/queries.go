e868b07d11a6ff502b14f9b0b82f1c658101eac3
fdf@mail.ru <fdf@mail.ru>
2020-03-08 12:51:58 +0500

    added

--- pkg/crud/services/burgers/queries.go
+++ pkg/crud/services/burgers/queries.go
@@ -3,10 +3,9 @@ package burgers
 const removeBurger = "UPDATE burgers SET removed = true where id = $1;"
 const saveInBurgers = "INSERT INTO burgers(name, price) VALUES ($1, $2);"
 const getFalseBurgers = "SELECT id, name, price FROM burgers WHERE removed = FALSE"
-const createDB = `CREATE TABLE burgers (
+const createDB = `CREATE TABLE  if not exists  burgers (
    id BIGSERIAL PRIMARY KEY,
    name TEXT NOT NULL,
    price INT NOT NULL CHECK ( price > 0 ),
-   -- don't remove any data from db
    removed BOOLEAN DEFAULT FALSE
 );`
